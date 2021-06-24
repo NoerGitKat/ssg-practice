68f3ea1b413e69c72f6584e7b578f25d78f6acd4
Noer Paanakker <noer@lightworksweb.nl>
2021-06-24 18:09:43 +0200

    get articles from different countries

--- src/_data/getNews.js
+++ src/_data/getNews.js
@@ -1,17 +1,25 @@
 const fetch = require("node-fetch");
+const countries = require("./countries.json");
 require("dotenv").config();
 
-const getNews = async () => {
+const getNewsByCountry = async (country) => {
   try {
     const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
+      `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}`
     );
     const news = await response.json();
 
-    return news;
+    return { country: country, articles: news.articles };
   } catch (error) {
     console.error(error);
   }
+};
+
+const getNews = async () => {
+  const news = countries.map(getNewsByCountry);
+  const newsObjects = await Promise.all(news);
+
+  return newsObjects;
 };
 
 module.exports = getNews;
